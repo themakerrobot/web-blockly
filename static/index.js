dc1df2ae1ccfb0bf89acaef19c31058b407d0eb4
THE MAKER <75306259+themakerrobot@users.noreply.github.com>
2024-07-19 15:00:31 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,95 +1,56 @@
 window.onload = function() {
-    const textEncoder = new TextEncoderStream();
-    const textDecoder = new TextDecoderStream();
-    const reader = textDecoder.readable.getReader();
-    const writer = textEncoder.writable.getWriter();
     const command = document.getElementById('command');
     const result = document.getElementById('result');
-    const send = document.getElementById('send');
+    const run = document.getElementById('run');
     const view = document.getElementById('view');
     const stop = document.getElementById('stop');
 
-
-let editor;
-
-
-  editor = CodeMirror(document.getElementById("python-code"), {
-    mode: "python",
-    theme: "monokai",
-    lineNumbers: true,
-    readOnly: true
-  });
-
-view.addEventListener('click', async () => {
-  const code = Blockly.Python.workspaceToCode(workspace);
-  console.log(code);
-  editor.setValue(code);
-});
-
+    let editor = CodeMirror(document.getElementById("python-code"), {
+        mode: "python",
+        theme: "monokai",
+        lineNumbers: true,
+        readOnly: true
+      });
     
-    // view.addEventListener('click', async () => {
-    //   console.log(Blockly.Python.workspaceToCode(workspace));
-    //   document.getElementById("python-code").innerText = Blockly.Python.workspaceToCode(workspace);
-    // });
+    view.addEventListener('click', async () => {
+      const code = Blockly.Python.workspaceToCode(workspace);
+      console.log(code);
+      editor.setValue(code);
+    });
 
-    send.addEventListener('click', async () => {
+    run.addEventListener('click', async () => {
       codetext = Blockly.Python.workspaceToCode(workspace);
       console.log(codetext)
-      await writer.write(codetext);
-      send.disabled = true;
-      document.getElementById("output").innerText = '';
-            
-      setTimeout(()=> {
-        send.disabled = false;
-      }, 3000);
+
+      Sk.execLimit = 60 * 1000;
+
+    
+      Sk.configure({
+        output: function (text) {
+            document.getElementById("output-content").innerText += text.replace(/^\n+/, ''); // 공백 제거
+        },
+        read: function (filename) {
+            if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][filename] === undefined) {
+                throw "File not found: '" + filename + "'";
+            }
+            return Sk.builtinFiles["files"][filename];
+          }
+      });
+    
+      (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'turtle';
+    
+      Sk.misceval.asyncToPromise(function () {
+        return Sk.importMainWithBody("<stdin>", false, code, true);
+      }).then(function (mod) {
+        document.getElementById("output-content").innerText += '\n\nEnd.';
+      }).catch(function (err) {
+        document.getElementById("output-content").innerText = err.toString();
+      });
     });
     
     stop.addEventListener('click', async () => {
-      await writer.write('###END###');
-      //writer.releaseLock();
+      Sk.execLimit = 0; 
     });
-    
-    document.getElementById('connect').addEventListener('click', async () => {
-    /*
-     const filters = [
-      { usbVendorId: 0x2341, usbProductId: 0x0043 },
-      { usbVendorId: 0x2341, usbProductId: 0x0001 }
-    ];
-    const port = await navigator.serial.requestPort({ filters });
-    */
-    const port = await navigator.serial.requestPort();
-    const { productId, vendorId } = port.getInfo();
-    console.log('Machine:', productId, vendorId);
-
-    await port.open({ baudRate: 115200 });
-    const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-    const writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
-    
-    try {
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-          // 스트림이 끝났을 때
-          console.log("Serial port closed");
-          break;
-        }
-        // 받은 데이터 처리
-        if (value) {
-          document.getElementById("output").innerText += value;
-          console.log("Received:", value);
-        }
-      }
-    } catch (error) {
-      console.error("Error reading from serial port:", error);
-    } finally {
-      reader.releaseLock();
-      writer.releaseLock();
-      await port.close();
-    }  
-  });
-
-if ("serial" in navigator) console.log("Your browser supports Web Serial API!");
-else document.getElementById("output").innerText = alert("Your browser does not support Web Serial API, the latest version of Google Chrome is recommended!");
 
 let CURRENT_DIR;
 let CODE_PATH = '';
